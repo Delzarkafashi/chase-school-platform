aec0cc33660f40989bcce7a50d1c8648eb16f733
Delzarkafashi <145854766+Delzarkafashi@users.noreply.github.com>
2026-08-31 11:28:11 +0200

    feat: add about content type

---
+++ Frontend/src/types/AboutContent.ts
@@ -0,0 +1,10 @@
+export type AboutContent = {
+  id: string;
+  section: string;
+  eyebrow: string;
+  title: string;
+  text: string;
+  image: string;
+  quote?: string;
+  quoteAuthor?: string;
+};
