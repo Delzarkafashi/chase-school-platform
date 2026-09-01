9ead0056fe35ae4c6f13f628edaf20a2b86c1149
Delzarkafashi <145854766+Delzarkafashi@users.noreply.github.com>
2026-09-01 10:41:15 +0200

    feat: build business page

--- Frontend/src/pages/BusinessPage.tsx
+++ Frontend/src/pages/BusinessPage.tsx
@@ -1,12 +1,56 @@
+import { useEffect, useState } from "react";
 import Header from "../components/layout/Header";
+import BusinessSection from "../components/business/BusinessSection";
+import type { BusinessContent } from "../types/BusinessContent";
+import { getBusinessContent } from "../api/businessApi";
+import "../styles/business-page.css";
 
 function BusinessPage() {
+  const [content, setContent] = useState<BusinessContent[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    async function loadBusinessContent() {
+      try {
+        const data = await getBusinessContent();
+        setContent(data);
+      } catch {
+        setError("Kunde inte hämta information för företag.");
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    loadBusinessContent();
+  }, []);
+
   return (
     <>
       <Header />
 
-      <main>
-        <h1>För företag</h1>
+      <main className="business-page">
+        {loading && (
+          <div className="business-page-container">
+            <p>Laddar...</p>
+          </div>
+        )}
+
+        {error && (
+          <div className="business-page-container">
+            <p>{error}</p>
+          </div>
+        )}
+
+        {!loading &&
+          !error &&
+          content.map((section, index) => (
+            <BusinessSection
+              key={section.id}
+              content={section}
+              index={index}
+            />
+          ))}
       </main>
     </>
   );
